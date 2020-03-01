48140ff2120a6e8d0b7d62ddfcc2ed3bd4fa1ad1
Siju <sijus887@gmail.com>
2020-03-01 10:45:42 +0530

    Fixed date logic incorrect in Recent for Yesterday, This Week
    This issue happens if date is 1st of month. So, it was not showing last
    month files eventhough they were taken a day before.
    Fix was to remove checking the month and year. Instead, check the time
    difference.

--- app/src/main/java/com/siju/acexplorer/helper/DateUtils.kt
+++ app/src/main/java/com/siju/acexplorer/helper/DateUtils.kt
@@ -2,14 +2,14 @@ package com.siju.acexplorer.helper
 
 import java.util.*
 
+private const val WEEK_TIME_MS = (7 * 24 * 3600 * 1000).toLong()
+private const val ONE_DAY_TIME_MS = (24 * 3600 * 1000).toLong()
+
 object DateUtils {
+
     fun isYesterday(ms: Long): Boolean {
         val now = Calendar.getInstance()
-        val date = Calendar.getInstance()
-        date.timeInMillis = ms
-        now.add(Calendar.DATE, -1)
-        return now[Calendar.YEAR] == date[Calendar.YEAR] && now[Calendar.MONTH] == date[Calendar.MONTH]
-                && now[Calendar.DATE] == date[Calendar.DATE]
+        return now.timeInMillis - ms <= ONE_DAY_TIME_MS
     }
 
     fun isToday(ms: Long): Boolean {
@@ -23,10 +23,7 @@ object DateUtils {
 
     fun isThisWeek(ms: Long): Boolean {
         val now = Calendar.getInstance()
-        val date = Calendar.getInstance()
-        date.timeInMillis = ms
-        return now[Calendar.YEAR] == date[Calendar.YEAR] && now[Calendar.MONTH] == date[Calendar.MONTH]
-                && now[Calendar.WEEK_OF_YEAR] == date[Calendar.WEEK_OF_YEAR]
+        return now.timeInMillis - ms <= WEEK_TIME_MS
     }
 
     fun isThisMonth(ms: Long): Boolean {
