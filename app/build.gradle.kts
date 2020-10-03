22dd50b5c2f374eb33e13b78fdc1dc19cb12b3c0
Siju <sijus887@gmail.com>
2020-10-03 12:21:19 +0530

    Add debug keystore info to allow Bitbucket pipeline to work

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,3 @@
-
-
 import java.io.FileInputStream
 import java.util.*
 
@@ -17,17 +15,31 @@ plugins {
 
 android {
     signingConfigs {
-        if (rootProject.file("keystore.properties").exists()) {
-            val signingRelease = Properties()
-            signingRelease.load(FileInputStream(rootProject.file("keystore.properties")))
+        getByName("debug") {
+            storeFile = rootProject.file("debug.keystore")
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+        }
+        val keyProperties = Properties()
+        try {
+            keyProperties.load(FileInputStream(rootProject.file("keystore.properties")))
             create("release") {
-                storeFile =  rootProject.file(signingRelease.getProperty("storeFile"))
-                storePassword = signingRelease.getProperty("storePassword")
-                keyAlias = signingRelease.getProperty("keyAlias")
-                keyPassword = signingRelease.getProperty("keyPassword")
+                storeFile = rootProject.file(keyProperties.getProperty("storeFile"))
+                storePassword = keyProperties.getProperty("storePassword")
+                keyAlias = keyProperties.getProperty("keyAlias")
+                keyPassword = keyProperties.getProperty("keyPassword")
+            }
+        } catch (exception: java.io.IOException) {
+            create("release") {
+                storeFile = rootProject.file("release.keystore")
+                storePassword = System.getenv("ANDROID_KEYSTORE_PASSWORD")
+                keyAlias = System.getenv("ANDROID_KEYSTORE_ALIAS")
+                keyPassword = System.getenv("ANDROID_KEYSTORE_PRIVATE_KEY_PASSWORD")
             }
         }
     }
+
 
     defaultConfig {
         applicationId = AndroidSdk.applicationId
@@ -38,8 +50,8 @@ android {
         versionName = rootProject.extra["versionName"] as String
         testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
         resConfigs("en", "af", "ar", "bg", "da", "de", "el", "es", "fa", "fr", "ga", "hi", "hr", "hu",
-        "hy", "in", "is", "it", "iw", "ja", "ka", "ko", "mk", "nl", "no", "pl", "pt", "ro", "ru",
-        "sl", "sq", "sr", "sv", "th", "tr", "uk", "vi", "zh", "zh-rCN", "zh-rTW")
+                "hy", "in", "is", "it", "iw", "ja", "ka", "ko", "mk", "nl", "no", "pl", "pt", "ro", "ru",
+                "sl", "sq", "sr", "sv", "th", "tr", "uk", "vi", "zh", "zh-rCN", "zh-rTW")
     }
 
 
@@ -51,6 +63,7 @@ android {
                 // you can speed up your build by disabling mapping file uploading.
                 mappingFileUploadEnabled = false
             }
+            signingConfig = signingConfigs.getByName("debug")
         }
 
 
@@ -71,9 +84,9 @@ android {
     }
 
     sourceSets {
-       getByName("main").res.srcDirs(
-               Libraries.ResDirs.directories
-       )
+        getByName("main").res.srcDirs(
+                Libraries.ResDirs.directories
+        )
     }
 
 
@@ -81,10 +94,10 @@ android {
     productFlavors {
         create("dev") {
             applicationIdSuffix = ".dev"
-            buildConfigField ("boolean", "IS_DEV_VERSION", "true")
+            buildConfigField("boolean", "IS_DEV_VERSION", "true")
         }
         create("prod") {
-            buildConfigField ("boolean", "IS_DEV_VERSION", "false")
+            buildConfigField("boolean", "IS_DEV_VERSION", "false")
         }
     }
     lintOptions {
@@ -99,15 +112,15 @@ android {
     sonarqube {
         properties {
             property("sonar.projectName", "ace-explorer")
-            property ("sonar.projectKey", "ace-explorer")
-            property ("sonar.language", "kotlin")
-            property ("sonar.sources", "src/main/java/")
-            property ("sonar.tests", "src/test/java/")
-            property ("sonar.binaries", "build")
-            property ("sonar.sourceEncoding", "UTF-8")
-            property ("sonar.login", "admin")
-            property ("sonar.password", "admin")
-            property ("sonar.host.url", "http://localhost:9000")
+            property("sonar.projectKey", "ace-explorer")
+            property("sonar.language", "kotlin")
+            property("sonar.sources", "src/main/java/")
+            property("sonar.tests", "src/test/java/")
+            property("sonar.binaries", "build")
+            property("sonar.sourceEncoding", "UTF-8")
+            property("sonar.login", "admin")
+            property("sonar.password", "admin")
+            property("sonar.host.url", "http://localhost:9000")
         }
     }
 
@@ -120,63 +133,63 @@ dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.aar"))))
     implementation(Libraries.ExternalLibs.rootTools) {
-         exclude(module = "RootShell")
+        exclude(module = "RootShell")
     }
-    implementation (Libraries.ExternalLibs.rootShell)
-    implementation (project(":billingsecure"))
+    implementation(Libraries.ExternalLibs.rootShell)
+    implementation(project(":billingsecure"))
 
-    implementation (Libraries.Kotlin.stdlib)
-    implementation (Libraries.Kotlin.coroutineCore)
-    implementation (Libraries.Kotlin.coroutineAndroid)
+    implementation(Libraries.Kotlin.stdlib)
+    implementation(Libraries.Kotlin.coroutineCore)
+    implementation(Libraries.Kotlin.coroutineAndroid)
 
-    implementation (Libraries.viewModelKtx)
-    implementation (Libraries.lifecycleLiveData)
+    implementation(Libraries.viewModelKtx)
+    implementation(Libraries.lifecycleLiveData)
     implementation(Libraries.lifecycleRuntime)
     implementation(Libraries.constraintLayout)
     kapt(Libraries.lifecycleCompiler)
 
-    implementation (Libraries.hilt)
+    implementation(Libraries.hilt)
     kapt(Libraries.hiltCompiler)
     implementation(Libraries.hiltViewModel)
     kapt(Libraries.hiltViewModelCompiler)
 
-    implementation (Libraries.roomRuntime)
-    kapt  (Libraries.roomCompiler)
+    implementation(Libraries.roomRuntime)
+    kapt(Libraries.roomCompiler)
 
-    // Support libraries
-    implementation (Libraries.appCompat)
-    implementation (Libraries.recyclerView)
-    implementation (Libraries.design)
-    implementation (Libraries.viewpager)
-    implementation (Libraries.cardView)
-    implementation (Libraries.palette)
-    implementation (Libraries.preference)
-    implementation (Libraries.navigation)
-    implementation (Libraries.navigationUi)
-    kapt (Libraries.annotation)
+// Support libraries
+    implementation(Libraries.appCompat)
+    implementation(Libraries.recyclerView)
+    implementation(Libraries.design)
+    implementation(Libraries.viewpager)
+    implementation(Libraries.cardView)
+    implementation(Libraries.palette)
+    implementation(Libraries.preference)
+    implementation(Libraries.navigation)
+    implementation(Libraries.navigationUi)
+    kapt(Libraries.annotation)
 
-    implementation (Libraries.Firebase.ads)
-    implementation (Libraries.Firebase.invites)
-    implementation (Libraries.Firebase.analytics)
+    implementation(Libraries.Firebase.ads)
+    implementation(Libraries.Firebase.invites)
+    implementation(Libraries.Firebase.analytics)
 
-    implementation (Libraries.billing)
-    implementation (Libraries.billingKtx)
-    implementation (Libraries.ExternalLibs.gson)
-    implementation (Libraries.playCore)
+    implementation(Libraries.billing)
+    implementation(Libraries.billingKtx)
+    implementation(Libraries.ExternalLibs.gson)
+    implementation(Libraries.playCore)
 
-    implementation  (Libraries.ExternalLibs.glideRuntime)
-    kapt (Libraries.ExternalLibs.glideCompiler)
+    implementation(Libraries.ExternalLibs.glideRuntime)
+    kapt(Libraries.ExternalLibs.glideCompiler)
 
-    implementation (Libraries.ExternalLibs.apacheCompress)
-    implementation (Libraries.ExternalLibs.floatingButton)
-    implementation (Libraries.ExternalLibs.rateApp)
-    implementation (Libraries.ExternalLibs.photoView)
-    implementation (Libraries.ExternalLibs.fastScrollRecyclerview)
+    implementation(Libraries.ExternalLibs.apacheCompress)
+    implementation(Libraries.ExternalLibs.floatingButton)
+    implementation(Libraries.ExternalLibs.rateApp)
+    implementation(Libraries.ExternalLibs.photoView)
+    implementation(Libraries.ExternalLibs.fastScrollRecyclerview)
 
-    debugImplementation (Libraries.ExternalLibs.leakCanary)
+    debugImplementation(Libraries.ExternalLibs.leakCanary)
 
-    implementation (Libraries.ExternalLibs.crashlytics)
+    implementation(Libraries.ExternalLibs.crashlytics)
 
-    testImplementation (Libraries.TestLibs.junit)
-    testImplementation (Libraries.TestLibs.mockito)
+    testImplementation(Libraries.TestLibs.junit)
+    testImplementation(Libraries.TestLibs.mockito)
 }
