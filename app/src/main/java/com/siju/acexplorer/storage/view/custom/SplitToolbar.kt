7d110bd38eda66c5e68b7a4076a190dd333e2a8d
Siju <sijus887@gmail.com>
2021-01-26 16:09:52 +0530

    Update to Studio 4.1.2
    - Update gradle plugin to 4.1.2 and wrapper to 6.8.1
    - Fix lint error

--- app/src/main/java/com/siju/acexplorer/storage/view/custom/SplitToolbar.kt
+++ app/src/main/java/com/siju/acexplorer/storage/view/custom/SplitToolbar.kt
@@ -23,9 +23,9 @@ import androidx.appcompat.widget.ActionMenuView
 import androidx.appcompat.widget.Toolbar
 
 class SplitToolbar : Toolbar {
-    constructor(context: Context?) : super(context)
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+    constructor(context: Context?) : super(context!!)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context!!, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context!!, attrs, defStyleAttr)
 
     override fun addView(child: View, params: ViewGroup.LayoutParams) {
         if (child is ActionMenuView) {
