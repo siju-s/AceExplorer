e492062035518819bc144c698c59b19c71aac0b3
Siju Sakarias <sijus887@gmail.com>
2024-05-25 14:21:11 -0500

    Upgrade Java version to 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -135,11 +135,11 @@ android {
     }
 
     kotlin {
-        jvmToolchain(17)
+        jvmToolchain(21)
     }
     compileOptions.apply {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     lint {
         //isCheckDependencies = true
--- feature/appmanager/build.gradle.kts
+++ feature/appmanager/build.gradle.kts
@@ -22,11 +22,11 @@ android {
         }
     }
     kotlin {
-        jvmToolchain(17)
+        jvmToolchain(21)
     }
     compileOptions.apply {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     buildFeatures {
